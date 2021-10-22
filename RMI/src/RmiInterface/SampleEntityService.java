7cab00cb7f4a7ef04aad3c88abe0ffe7acea50aa
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-10-22 10:43:17 +0700

    Delete Sample Java File

--- RMI/src/RmiInterface/SampleEntityService.java
+++ RMI/src/RmiInterface/SampleEntityService.java
@@ -1,11 +1,8 @@
 package RmiInterface;
 
 import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-import RmiDto.SampleEntityInfoDTO;
 
 public interface SampleEntityService extends Remote{
 	
-	public boolean AddNewThietBi (SampleEntityInfoDTO tb ) throws RemoteException;
+//	public boolean AddNewThietBi (SampleEntityInfoDTO tb ) throws RemoteException;
 }
