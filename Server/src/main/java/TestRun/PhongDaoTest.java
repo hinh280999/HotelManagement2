9221014e183f517087ad475e8d3d012d7991dbaf
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-10-23 20:26:26 +0700

    add file PhongDaoTest

---
+++ Server/src/main/java/TestRun/PhongDaoTest.java
@@ -0,0 +1,45 @@
+package TestRun;
+
+import Dao.Impliment.PhongDao;
+import Dao.Interface.IPhongDao;
+import Entity.Phong;
+
+public class PhongDaoTest {
+
+	public static void main(String[] args) {
+//		IPhongDao Dao = new PhongDao();
+//
+//		Phong tPhong = new Phong("P2809");
+//		tPhong.setMaTTP(null);
+//		tPhong.setMaLP(null);
+//
+//		if (Dao.add(tPhong)) {
+//			System.out.println("Add Phong Complete");
+//		} else {
+//			System.out.println("Add Phong Fail");
+//		}
+//
+//		Phong pInfo = null;
+//		try {
+//			pInfo = Dao.get(tPhong.getMaP());
+//			System.out.println(pInfo.toString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		pInfo.setTen("Pepsi");
+//		if (Dao.update(pInfo)) {
+//			System.out.println("update Phong Complete");
+//		} else {
+//			System.out.println("update Phong Fail");
+//		}
+//
+//		if (Dao.delete(pInfo)) {
+//			System.out.println("delete Phong Complete");
+//		} else {
+//			System.out.println("delete Phong Fail");
+//		}
+
+	}
+
+}
