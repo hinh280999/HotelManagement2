b047aa8fbf0312f0f10aeb96834b2319db5f66d4
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-10-23 20:47:03 +0700

    Add LoaiPhongDao

---
+++ Server/src/main/java/Dao/Impliment/LoaiPhongDao.java
@@ -0,0 +1,129 @@
+package Dao.Impliment;
+
+import java.util.List;
+
+import org.hibernate.Transaction;
+import org.hibernate.ogm.OgmSession;
+import org.hibernate.ogm.OgmSessionFactory;
+
+import Dao.Interface.ILoaiPhong;
+import Entity.LoaiPhong;
+import Entity.Phong;
+import Utilities.HibernateUtil;
+
+public class LoaiPhongDao implements ILoaiPhong {
+	private OgmSessionFactory sessionFactory;
+
+	public LoaiPhongDao() {
+		sessionFactory = HibernateUtil.getInstance().getSessionFactory();
+	}
+
+	@Override
+	public LoaiPhong get(int id) throws Exception {
+		if (id <= 0)
+			throw new Exception("id < 0 is not allow");
+
+		OgmSession session = sessionFactory.getCurrentSession();
+		Transaction tr = session.beginTransaction();
+
+		try {
+			LoaiPhong temp = session.find(LoaiPhong.class, id);
+
+			tr.commit();
+			session.close();
+
+			return temp;
+		} catch (Exception e) {
+			tr.rollback();
+			session.close();
+
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public List<LoaiPhong> getAll() {
+		OgmSession session = sessionFactory.getCurrentSession();
+		Transaction tr = session.beginTransaction();
+		String query = "db.loaiphongs.find({})";
+		try {
+			List<LoaiPhong> list = session.createNativeQuery(query, LoaiPhong.class)
+					.getResultList();
+			tr.commit();
+			session.close();
+
+			return list;
+		} catch (Exception e) {
+			tr.rollback();
+			session.close();
+
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean add(LoaiPhong addObject) {
+		OgmSession session = sessionFactory.getCurrentSession();
+		Transaction tr = session.beginTransaction();
+		try {
+			session.save(addObject);
+
+			tr.commit();
+			session.close();
+
+			return true;
+		} catch (Exception e) {
+			tr.rollback();
+			session.close();
+
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean update(LoaiPhong updateObject) {
+		OgmSession session = sessionFactory.getCurrentSession();
+		Transaction tr = session.beginTransaction();
+		try {
+			session.saveOrUpdate(updateObject);
+
+			tr.commit();
+			session.close();
+
+			return true;
+		} catch (Exception e) {
+			tr.rollback();
+			session.close();
+
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean delete(LoaiPhong deleteObject) {
+		OgmSession session = sessionFactory.getCurrentSession();
+		Transaction tr = session.beginTransaction();
+		try {
+			session.delete(deleteObject);
+
+			tr.commit();
+			session.close();
+
+			return true;
+		} catch (Exception e) {
+			tr.rollback();
+			session.close();
+
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+}
---
+++ Server/src/main/java/Dao/Interface/ILoaiPhong.java
@@ -0,0 +1,7 @@
+package Dao.Interface;
+
+import Entity.LoaiPhong;
+
+public interface ILoaiPhong extends IBaseDao<LoaiPhong>{
+
+}
