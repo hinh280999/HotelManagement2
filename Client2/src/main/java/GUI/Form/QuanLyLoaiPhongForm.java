a52537f94d7239e833216445ae5747c50c267acb
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-11-28 16:28:59 +0700

    Update variable QuanLyKH form

--- Client2/src/main/java/GUI/Form/QuanLyLoaiPhongForm.java
+++ Client2/src/main/java/GUI/Form/QuanLyLoaiPhongForm.java
@@ -2,6 +2,8 @@ package GUI.Form;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -13,19 +15,25 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 
 import CustomControll.ColorButton2;
+import Model.PageList;
+import Rmi.DTO.KhachHangDTO;
+import Rmi.DTO.LoaiPhongDTO;
 
-public class QuanLyLoaiPhongForm extends JPanel {
+public class QuanLyLoaiPhongForm extends JPanel implements ActionListener {
 
-	private ColorButton2 btnThemKhachHang;
-	private ColorButton2 btnSuaKhachHang;
-	private ColorButton2 btnXoaKhachHang;
+	private ColorButton2 btnThem;
+	private ColorButton2 btnSua;
+	private ColorButton2 btnXoa;
 	private JTextField txtSearchText;
 	private JButton btnSearch;
 	private JTable tblDsKhachHang;
-	protected Object lstKhachHang;
 	private JButton btnPrev;
 	private JLabel lblPage;
 	private JButton btnNext;
+	private PageList<LoaiPhongDTO> lstLoaiPhong;
+	private LoaiPhongDTO selectedLoaiPhong = null;
+	private int currentPage, maxPage;
+	private static int maxRow = 15;
 
 	public QuanLyLoaiPhongForm() {
 		setBackground(Color.decode("#d4d5d6"));
@@ -38,23 +46,23 @@ public class QuanLyLoaiPhongForm extends JPanel {
 		add(pSearch);
 		pSearch.setLayout(null);
 
-		btnThemKhachHang = new ColorButton2(Color.decode("#34e039"), Color.decode("#38f53e"), Color.decode("#32cf37"),
+		btnThem = new ColorButton2(Color.decode("#34e039"), Color.decode("#38f53e"), Color.decode("#32cf37"),
 				Color.decode("#34e039"));
-		btnThemKhachHang.setText("Thêm Loại Phòng");
-		btnThemKhachHang.setBounds(10, 80, 100, 30);
-		pSearch.add(btnThemKhachHang);
+		btnThem.setText("Thêm Loại Phòng");
+		btnThem.setBounds(10, 80, 100, 30);
+		pSearch.add(btnThem);
 
-		btnSuaKhachHang = new ColorButton2(Color.decode("#f0f03a"), Color.decode("#fafa3c"), Color.decode("#e0e034"),
+		btnSua = new ColorButton2(Color.decode("#f0f03a"), Color.decode("#fafa3c"), Color.decode("#e0e034"),
 				Color.decode("#f0f03a"));
-		btnSuaKhachHang.setBounds(120, 80, 150, 30);
-		btnSuaKhachHang.setText("Sửa Loại Phòng");
-		pSearch.add(btnSuaKhachHang);
+		btnSua.setBounds(120, 80, 150, 30);
+		btnSua.setText("Sửa Loại Phòng");
+		pSearch.add(btnSua);
 
-		btnXoaKhachHang = new ColorButton2(Color.decode("#ed3752"), Color.decode("#ff425e"), Color.decode("#e63c55"),
+		btnXoa = new ColorButton2(Color.decode("#ed3752"), Color.decode("#ff425e"), Color.decode("#e63c55"),
 				Color.decode("#ed3752"));
-		btnXoaKhachHang.setBounds(280, 80, 150, 30);
-		btnXoaKhachHang.setText("Xóa Loại Phòng");
-		pSearch.add(btnXoaKhachHang);
+		btnXoa.setBounds(280, 80, 150, 30);
+		btnXoa.setText("Xóa Loại Phòng");
+		pSearch.add(btnXoa);
 
 		txtSearchText = new JTextField();
 		txtSearchText.setBounds(10, 10, 310, 30);
@@ -80,7 +88,7 @@ public class QuanLyLoaiPhongForm extends JPanel {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				int selectedRow = tblDsKhachHang.getSelectedRow();
-				//selectedKhachHang = lstKhachHang.getListData().get(selectedRow);
+				//selectedKhachHang = lstLoaiPhong.getListData().get(selectedRow);
 		}});
 		scrollPane.setViewportView(tblDsKhachHang);
 
@@ -100,5 +108,19 @@ public class QuanLyLoaiPhongForm extends JPanel {
 		btnNext = new JButton(">");
 		btnNext.setBounds(500, 589, 85, 21);
 		pTable.add(btnNext);
+		
+		// === Add acction ====== 
+		btnPrev.addActionListener(this);
+		btnNext.addActionListener(this);
+		btnThem.addActionListener(this);
+		btnSua.addActionListener(this);
+		btnXoa.addActionListener(this);
+		btnSearch.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
