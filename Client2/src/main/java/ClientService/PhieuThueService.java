7146379bc71b4ca3773d0c4f689d7ccc7b92a857
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-11-29 10:13:14 +0700

    Add PhieuThueService to Client2

---
+++ Client2/src/main/java/ClientService/PhieuThueService.java
@@ -0,0 +1,34 @@
+package ClientService;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import Rmi.Interface.IPhieuThueService;
+
+public class PhieuThueService {
+	private IPhieuThueService phieuThueService;
+	private static PhieuThueService instance = null;
+
+	private PhieuThueService() throws MalformedURLException, RemoteException, NotBoundException {
+		SecurityManager manager = System.getSecurityManager();
+		if (manager == null) {
+			System.setProperty("java.security.policy", "rmi/policy.policy");
+			System.setSecurityManager(new SecurityManager());
+		}
+		phieuThueService = (IPhieuThueService) Naming
+				.lookup(RmiConstant.ServerPath + ":" + RmiConstant.ServerPort + "/phieuThueService");
+	}
+
+	public synchronized static PhieuThueService getInstance() {
+		if (instance == null) {
+			try {
+				instance = new PhieuThueService();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return instance;
+	}
+}
