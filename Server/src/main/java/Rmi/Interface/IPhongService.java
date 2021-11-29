781773f159546ef278ac87f06632bb8a74641e06
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-11-29 17:24:37 +0700

    Add getListPhongDaThue - ClientService

--- Server/src/main/java/Rmi/Interface/IPhongService.java
+++ Server/src/main/java/Rmi/Interface/IPhongService.java
@@ -9,4 +9,6 @@ public interface IPhongService extends IBaseService<PhongDTO> {
 	PhongDTO getPhongTrongByLoaiPhongId(int loaiPhongId) throws RemoteException;
 
 	PageList<PhongDTO> getListPhongByPage(int pageNumb, int maxRow, String roomName) throws RemoteException;
+
+	PageList<PhongDTO> getListPhongDaThue(int pageNumb, int maxRow, String roomName) throws RemoteException;
 }
